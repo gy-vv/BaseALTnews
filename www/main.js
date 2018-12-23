85f410c8f130b2d8b01d6bf0adefa7abd78285bf
gy-vv <45867708+gy-vv@users.noreply.github.com>
2018-12-23 13:50:05 +0300

    Update main.js

--- www/main.js
+++ www/main.js
@@ -55,10 +55,7 @@ $("#mainPage").live("pageinit", function() {
 //Listen for the content page to load
 $("#contentPage").live("pageshow", function(prepage) {
 	//set vibration
-	try {
-	navigator.notification.beep(1);
-	navigator.notification.vibrate(200);
-              }
+	try { navigator.vibrate(200) }
 	cath {//nothing to vibrate by}	
 	//Set the title
 	$("h1", this).text(entries[selectedEntry].title);
